4fde8a060ce5986c2d1be488d0568e67b8ced37b
Dwi Zulyan Hamid <pokokseneng.12@gmail.com>
2021-11-02 13:57:27 +0700

    add some new features

--- docs/app.js
+++ docs/app.js
@@ -1,8 +1,20 @@
 const btn = document.querySelector('.hamburger')
 const link = document.querySelector('.link')
 const item = document.querySelectorAll('.item')
+const line1 = document.querySelector('.line:nth-child(1)')
+const line2 = document.querySelector('.line:nth-child(2)')
+const line3 = document.querySelector('.line:nth-child(3)')
+const line = document.querySelector('.line')
+
+
+
+
 btn.addEventListener('click',function(){
     link.classList.toggle('active')
+    line.classList.toggle('margin0')
+    line1.classList.toggle('active1')
+    line2.classList.toggle('active2')
+    line3.classList.toggle('active3')
 })
 
 for (var click of item){
